aa0bd0064d4998ec6ebe830d4bfc6adadbfcdb8c
wesleylo <weslo404@gmail.com>
2017-04-28 05:39:50 -0400

    Go!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,93 +7,19 @@
 
         <title>BlueBox</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+          <div id="app">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    BlueBox
-                </div>
-                <div class="links">
-                    <a href="https://wesleylo.gitbooks.io/bluebox/content/">Documentation</a>
-                    <a href="https://github.com/wesleylo/bluebox-api">GitHub</a>
-                </div>
-                <div class="links" style="margin-top:70px;">
-                  <p>A RESTful API built with <a href="https://laravel.com/">Laravel</a></p>
-                </div>
-
-                <div class="links">
-                  <p>Copyright 2017 <a href="https://github.com/wesleylo">Wesley Lo</a></p>
-                </div>
-            </div>
+          </div>
+          <script>
+              window.Laravel = {!! json_encode([
+                  'csrfToken' => csrf_token(),
+              ]) !!};
+          </script>
+          <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+          <script src="{{ mix('js/app.js') }}"></script>
         </div>
-
     </body>
 </html>
